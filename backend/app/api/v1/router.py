1da81fc0803a882663281393a4f09b318514958b
mariyadymova06-star <mariyadymova06@gmail.com>
2026-06-13 21:46:22 +0300

    feat: апи для измерений

--- backend/app/api/v1/router.py
+++ backend/app/api/v1/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from app.api.v1 import auth, profile
+from app.api.v1 import auth, profile, measurements
 
 router = APIRouter(prefix="/api/v1")
 router.include_router(auth.router)
 router.include_router(profile.router)
+router.include_router(measurements.router)
