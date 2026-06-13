c7ef940b1a5e905938c33e4dda9b2066fe3309ec
mariyadymova06-star <mariyadymova06@gmail.com>
2026-06-13 21:45:27 +0300

    feat: эндпоинты для профиля

--- backend/app/api/v1/router.py
+++ backend/app/api/v1/router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
-from app.api.v1 import auth
+from app.api.v1 import auth, profile
 
 router = APIRouter(prefix="/api/v1")
 router.include_router(auth.router)
+router.include_router(profile.router)
